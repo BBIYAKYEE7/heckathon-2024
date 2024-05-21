9335c65414bf58d901bf9d9aae9babefa34c255f
BBIYAKYEE7 <dadalgojr@gmail.com>
2024-05-21 17:43:14 +0900

    Code delete

--- visionize/src/App.js
+++ visionize/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [opacity, setOpacity] = useState(0);
+  const [scale, setScale] = useState(3); // Start from a larger scale for a zoom out effect
+
+  useEffect(() => {
+    document.title = "Visionize";
+    const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = Logo;
+    document.getElementsByTagName('head')[0].appendChild(link);
+  }, []);
+
+  useEffect(() => {
+    const onScroll = () => {
+      let currentPosition = window.pageYOffset;
+      if (currentPosition > 50) {
+        setOpacity(1);
+        setScale(1); // Scale down to 1 when scrolled more than 50px
+      } else {
+        setOpacity(currentPosition / 50);
+        setScale(1 + (50 - currentPosition) / 50 * 2); // Scale down from 3 to 1 as you scroll
+      }
+      setScrollPosition(currentPosition);
+    };
+
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [scrollPosition]);
+
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundColor: 'black' }}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={Logo} className="App-logo" alt="logo" style={{ opacity: opacity, transform: `scale(${scale})`, position: 'fixed', top: `${scrollPosition}px` }}/>
       </header>
+      <div style={{ height: '2000px' }}></div>
     </div>
   );
 }
